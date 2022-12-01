5719db73ef1fb20b4a2e891ea5866b6e56a8afed
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-12-01 14:43:09 -0300

    criado view IndexView

--- core/views.py
+++ core/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+from .utils import get_client_data, yelp_search
+
+
+class IndexView(View):
+    
+    def get(self, request, *args, **kwargs):
+        items = []
+        city = None
+        
+        # loop até encontrar um ip que tenha nome de cidade (alguns ips gerados aleatoriamente nao retornam nome de cidade)
+        while not city:
+            ret = get_client_data()
+            city = ret['city']
+        
+        q = request.GET.get('key', None)
+        loc = request.GET.get('loc', None)
+        location = city
+        
+        context = {
+            'city': city,
+            'busca': False
+        }
+        
+        if loc:
+            location = loc
+        if q:
+            items = yelp_search(keyword=q, location=location)
+            context = {
+                'items': items,
+                'city': location,
+                'busca': True
+            }
+        
+        return render(request, 'index.html', context)
